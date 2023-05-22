e09cf948b64a4ab99031a5924659cfecf01ff920
aleksandrarup <rupasova.ab@gmail.com>
2023-05-22 16:21:35 +0300

    hw_2

---
+++ Практика 2/b_Самостоятельная + домашняя работа/a_create_ui.py
@@ -0,0 +1,67 @@
+import PySide6
+from PySide6 import QtWidgets, QtCore
+
+
+class Window(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initUi() # TODO Вызовите метод для инициализации интерфейса
+
+    def initUi(self) -> None:
+        """
+        Инициализация интерфейса
+
+        :return: None
+        """
+
+        labelLogin = QtWidgets.QLabel()
+        labelLogin.setText("Логин")   # TODO Создайте виджет QLabel с текстом "Логин"
+
+        labelRegistration = QtWidgets.QLabel()
+        labelRegistration.setText("Регистрация")   # TODO Создайте виджет QLabel с текстом "Регистрация"
+
+        self.lineEditLogin = QtWidgets.QLineEdit  # TODO создайте виджет QLineEdit
+        self.lineEditLogin.setPlaceholderText('Введите логин')
+        #  TODO добавьте placeholderText "Введите логин" с помощью метода .setPlaceholderText()
+
+        self.lineEditPassword = QtWidgets.QLineEdit  # TODO создайте виджет QLineEdit
+        self.lineEditPassword.setPlaceholderText('Введите пароль')
+        # TODO добавьте placeholderText "Введите пароль"
+        self.lineEditPassword.setEchoMode(QtWidgets.QLineEdit.EchoMode.PasswordEchoOnEdit)
+        # TODO установите ограничение видимости вводимых знаков с помощью метода .setEchoMode()
+
+        self.pushButtonLogin = QtWidgets.QPushButton  # TODO создайте виджет QPushButton
+        self.pushButtonLogin.setText("Войти")  # TODO установите текст "Войти" с помощью метода setText()
+
+        self.pushButtonRegistration = QtWidgets.QPushButton  # TODO создайте виджет QPushButton
+        self.pushButtonRegistration.setText("Регистрация")  # TODO установите текст "Регистрация" с помощью метода setText()
+
+        layoutLogin = QtWidgets.QHBoxLayout  # TODO Создайте QHBoxLayout
+        layoutLogin.addWidget(labelLogin)  # TODO с помощью метода .addWidget() добавьте labelLogin
+        layoutLogin.addWidget(self.lineEditLogin)  # TODO с помощью метода .addWidget() добавьте self.lineEditLogin
+
+        layoutPassword = QtWidgets.QHBoxLayout  # TODO Создайте QHBoxLayout
+        layoutPassword.addWidget(labelRegistration)  # TODO с помощью метода .addWidget() добавьте labelRegistration
+        layoutPassword.addWidget(self.lineEditPassword)  # TODO с помощью метода .addWidget() добавьте self.lineEditPassword
+
+        layoutButtons = QtWidgets.QHBoxLayout  # TODO Создайте QHBoxLayout
+        layoutButtons.addWidget(self.pushButtonLogin)  # TODO с помощью метода .addWidget() добавьте self.pushButtonLogin
+        layoutButtons.addWidget(self.pushButtonRegistration)  # TODO с помощью метода .addWidget() добавьте self.pushButtonRegistration
+
+        layoutMain = QtWidgets.QVBoxLayout  # TODO Создайте QVBoxLayout
+        layoutMain.addWidget(layoutLogin) # TODO с помощью метода .addLayout() добавьте layoutLogin
+        layoutMain.addWidget(layoutPassword)  # TODO с помощью метода .addLayout() добавьте layoutPassword
+        layoutMain.addWidget(layoutButtons)  # TODO с помощью метода .addLayout() добавьте layoutButtons
+
+        self.setLayout(layoutMain())  # TODO с помощью метода setLayout установите layoutMain на основной виджет
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    window = Window()
+    window.show()
+
+    app.exec()
---
+++ Практика 2/b_Самостоятельная + домашняя работа/c_signals_events.py
@@ -0,0 +1,54 @@
+"""
+Реализация программу проверки состояния окна:
+Форма для приложения (ui/c_signals_events.ui)
+
+Программа должна обладать следующим функционалом:
+
+1. Возможность перемещения окна по заданным координатам.
+2. Возможность получения параметров экрана (вывод производить в plainTextEdit + добавлять время).
+    * Кол-во экранов
+    * Текущее основное окно
+    * Разрешение экрана
+    * На каком экране окно находится
+    * Размеры окна
+    * Минимальные размеры окна
+    * Текущее положение (координаты) окна
+    * Координаты центра приложения
+    * Отслеживание состояния окна (свернуто/развёрнуто/активно/отображено)
+3. Возможность отслеживания состояния окна (вывод производить в консоль + добавлять время).
+    * При перемещении окна выводить его старую и новую позицию
+    * При изменении размера окна выводить его новый размер
+"""
+from c_sgs import Ui_Form
+
+from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
+
+
+class Window(QtWidgets.QWidget):
+@@ -30,6 +30,21 @@ def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.initSignals()
+
+    def initSignals(self):
+        self.ui.pushButtonLT.clicked.connect(self.GoTopLeft)
+        self.ui.pushButtonRT.clicked.connect(self.GoTopRight)
+
+    def GoTopLeft(self):
+        self.move(0, 0)
+
+    def GoTopRight(self):
+        self.move()
+
+
+
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+    window = Window()
+    window.show()
+    app.exec()
